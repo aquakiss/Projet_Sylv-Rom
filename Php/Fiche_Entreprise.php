167598abe849e5cbf736e9efe01f26786885c552
aquakiss <romain.menard17@gmail.com>
2015-04-08 09:01:35 +0200

    modif Fiche_Entreprise.php

--- Php/Fiche_Entreprise.php
+++ Php/Fiche_Entreprise.php
@@ -1,2 +1,19 @@
 <!-- Entreprise PAge 'fiche' -->
 
+<?php
+try
+	{
+		$connexion = new PDO("mysql:host=localhost;dbname=enterprise","root","");
+		$connexion->query("SET NAMES UTF8");
+	}
+	catch (PDOException $e)
+	{
+		echo 'Echec lors de la tentative de connexion à la Base de donées' . $e->getMessage();
+		die();
+	}
+//recupere tout la table contact par ordre decroissante des dates
+ $reponse = $connexion->prepare('SELECT nom  FROM entreprise ORDER BY domaine ASC');
+ $reponse->execute();
+// On affiche les resultats
+$donnees = $reponse->fetchAll();
+?>
